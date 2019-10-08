de8bb0c342255ae7a941f13904a49fad1916295c
MaggieWang0614 <maggiewang@asu.edu>
2019-10-08 08:21:46 -0700

    back up;

--- tests/test.R
+++ tests/test.R
@@ -1,10 +1,11 @@
 library(PBDE)
 
 x = 1
-x_vec = c(1)
+x_vec = as.matrix(rnorm(20, 2, 1))
+x_prior = as.matrix(rnorm(10, 0, 1))
 tau = 1
 mu = 0
 sigma = 1
 
-output = p_n(x, x_vec, tau, mu, sigma)
+output = p_n(x, x_vec, x_prior, tau)
 print(output$value)
